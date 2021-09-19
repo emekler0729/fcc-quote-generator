0700f9bf3b323346ff85331f70091b1886dceaac
Eduard <emekler0729@gmail.com>
2021-09-19 00:09:07 -0500

    improved styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const BACKGROUND_DARK = "#b88bbf";
+const darkBackground = "#b88bbf";
+const transitionTime = 500;
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
     height: 100vh;
-    display: grid;
-    place-items: center;
+
     background-color: #caa6cb;
 `;
 
-const QuoteBox = styled.div.attrs((props) => ({
-    id: "quote-box",
-}))`
-    background: ${BACKGROUND_DARK};
-    border: 4px solid white;
+const QuoteBox = styled.div`
+    position: relative;
+
     width: 80%;
+    min-width: 320px;
     max-width: 500px;
+    margin-bottom: 2rem;
     padding: 2rem;
+    border: 4px solid white;
+    box-sizing: border-box;
+
+    background: ${darkBackground};
 `;
 
-const Quote = styled.p.attrs(() => ({
-    id: "text",
-}))`
+const Quote = styled.div`
+    margin-bottom: 5rem;
+    overflow-wrap: break-word;
+`;
+
+const Text = styled.p`
+    font-size: ${(props) => (props.length > 220 ? "1.5rem" : "2rem")};
     color: white;
     text-align: center;
-    font-size: 1.25rem;
+
+    transition: opacity ${transitionTime}ms;
+
+    opacity: ${(props) => (props.visible ? "100%" : "0%")};
 `;
 
-const Author = styled(Quote).attrs(() => ({
-    id: "author",
-}))`
-    text-align: right;
+const Author = styled(Text)`
+    font-size: 1.5rem;
     font-weight: 300;
+    text-align: right;
 `;
 
 const Controls = styled.div`
@@ -41,25 +56,57 @@ const Controls = styled.div`
 `;
 
 const Button = styled.a`
-    background: white;
-    border: none;
     padding: 1rem;
-    cursor: pointer;
-    color: ${BACKGROUND_DARK};
+    border: none;
     border-radius: 5px;
-    text-decoration: none;
+
+    background: white;
+
     font-family: inherit;
-    font-size: 14px;
+    font-size: 16px;
+    font-weight: bold;
+    color: ${darkBackground};
+    text-decoration: none;
     line-height: 1;
+
+    transition: background 500ms;
+
+    cursor: pointer;
+
+    &:hover {
+        background: #eee;
+    }
+`;
+
+const Footer = styled.footer`
+    color: white;
+
+    a,
+    a:visited {
+        font-weight: bold;
+        color: white;
+        text-decoration: none;
+
+        transition: color 500ms;
+    }
+
+    a:hover {
+        color: #eee;
+    }
 `;
 
 const App = () => {
-    const [{ quote, author }, setQuote] = useState({ quote: null, author: null });
+    const [{ quote, author }, setQuote] = useState({ quote: "", author: "" });
+    const [transitioning, setTransitioning] = useState(true);
 
     async function updateQuote() {
+        setTransitioning(true);
         const response = await fetch("https://api.quotable.io/random");
         const data = await response.json();
-        return setQuote({ quote: data.content, author: data.author });
+        setTimeout(() => {
+            setQuote({ quote: data.content, author: data.author });
+            setTransitioning(false);
+        }, transitionTime);
     }
 
     useEffect(() => {
@@ -68,9 +115,15 @@ const App = () => {
 
     return (
         <Container>
-            <QuoteBox>
-                <Quote>{quote}</Quote>
-                <Author>—{author}</Author>
+            <QuoteBox id="quote-box">
+                <Quote>
+                    <Text visible={!transitioning} length={quote.length} id="text">
+                        {quote}
+                    </Text>
+                    <Author visible={!transitioning} id="author">
+                        —{author}
+                    </Author>
+                </Quote>
                 <Controls>
                     <Button
                         id="tweet-quote"
@@ -83,6 +136,12 @@ const App = () => {
                     </Button>
                 </Controls>
             </QuoteBox>
+            <Footer>
+                Designed and developed by{" "}
+                <a href="https://www.eduard-mekler.com" target="_blank" rel="noreferrer">
+                    Eduard Mekler
+                </a>
+            </Footer>
         </Container>
     );
 };

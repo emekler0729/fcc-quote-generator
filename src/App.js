0271eb130f827ee7ae49c1c32aca53e854c3dadc
Eduard <emekler0729@gmail.com>
2021-09-18 22:18:33 -0500

    random quote generator mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const BACKGROUND_DARK = "#b88bbf";
+
+const Container = styled.div`
+    height: 100vh;
+    display: grid;
+    place-items: center;
+    background-color: #caa6cb;
+`;
+
+const QuoteBox = styled.div.attrs((props) => ({
+    id: "quote-box",
+}))`
+    background: ${BACKGROUND_DARK};
+    border: 4px solid white;
+    width: 80%;
+    max-width: 500px;
+    padding: 2rem;
+`;
+
+const Quote = styled.p.attrs(() => ({
+    id: "text",
+}))`
+    color: white;
+    text-align: center;
+    font-size: 1.25rem;
+`;
+
+const Author = styled(Quote).attrs(() => ({
+    id: "author",
+}))`
+    text-align: right;
+    font-weight: 300;
+`;
+
+const Controls = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Button = styled.a`
+    background: white;
+    border: none;
+    padding: 1rem;
+    cursor: pointer;
+    color: ${BACKGROUND_DARK};
+    border-radius: 5px;
+    text-decoration: none;
+    font-family: inherit;
+    font-size: 14px;
+    line-height: 1;
+`;
+
+const App = () => {
+    const [{ quote, author }, setQuote] = useState({ quote: null, author: null });
+
+    async function updateQuote() {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        return setQuote({ quote: data.content, author: data.author });
+    }
+
+    useEffect(() => {
+        updateQuote();
+    }, []);
+
+    return (
+        <Container>
+            <QuoteBox>
+                <Quote>{quote}</Quote>
+                <Author>—{author}</Author>
+                <Controls>
+                    <Button
+                        id="tweet-quote"
+                        href={`https://www.twitter.com/intent/tweet?text=${`${quote} —${author}`}`}
+                    >
+                        <i className="bi bi-twitter"></i>
+                    </Button>
+                    <Button id="new-quote" as="button" onClick={updateQuote}>
+                        New Quote
+                    </Button>
+                </Controls>
+            </QuoteBox>
+        </Container>
+    );
+};
 
 export default App;
